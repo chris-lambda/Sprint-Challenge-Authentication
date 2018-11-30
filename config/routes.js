211775ec9425e2ca6530cba5f4e5a34a475c9424
Stublink <chrisrey95@gmail.com>
2018-11-30 12:05:42 -0500

    :zap: added register endpoint functioning with jwt

--- config/routes.js
+++ config/routes.js
@@ -1,6 +1,12 @@
 const axios = require('axios');
+const bcrypt = require('bcryptjs')
+const jwt = require('jsonwebtoken');
+
 
 const { authenticate } = require('./middlewares');
+const db = require('../database/dbConfig');
+
+const key = require('../_secrets/keys')
 
 module.exports = server => {
   server.post('/api/register', register);
@@ -8,8 +14,38 @@ module.exports = server => {
   server.get('/api/jokes', authenticate, getJokes);
 };
 
+const generateToken = (user) => {
+
+  const payload = {
+      subject: user.userId,
+      username: user.username,
+  }
+
+  const secret = key.jwtKey;
+  const options = {
+      expiresIn: '1h'
+  }
+
+  return jwt.sign(payload, secret, options)
+}
+
 function register(req, res) {
   // implement user registration
+  const creds = req.body;
+
+    if(!creds.username || !creds.password) {
+        res.status(422).json({message: 'username and password both required'});
+        return;
+    }
+    creds.password = bcrypt.hashSync(creds.password, 8);
+
+    db('users')
+        .insert(creds)
+        .then(ids => {
+            const token = generateToken(creds);
+            res.status(201).json({ids, token})
+        })
+        .catch(err => res.json(err));
 }
 
 function login(req, res) {
